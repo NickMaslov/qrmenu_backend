df9053c3cb96309d84e149f37ab371268ced3558
Nick Maslov <kategorychnost@gmail.com>
2022-05-17 17:36:29 -0400

    created OrderSerializer, views for OrderList and OrderDetail, + urls

--- qrmenu_backend/urls.py
+++ qrmenu_backend/urls.py
@@ -20,5 +20,8 @@ urlpatterns = [
 
     path('api/create_payment_intent/', views.create_payment_intent),
 
+    path('api/orders/', views.OrderList.as_view()),
+    path('api/orders/<pk>', views.OrderDetail.as_view()),
+
 
 ]
